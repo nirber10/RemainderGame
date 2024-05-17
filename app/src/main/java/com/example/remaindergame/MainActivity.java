7e5500f14dd7a4a1e8be69e549f001db52643acc
NirBerman-app <dhdhggffv@gmail.com>
2024-05-17 11:22:53 +0300

    click

--- app/src/main/java/com/example/remaindergame/MainActivity.java
+++ app/src/main/java/com/example/remaindergame/MainActivity.java
@@ -1,51 +1,28 @@
 package com.example.remaindergame;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.imageview.ShapeableImageView;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    int count = 0;
-    int card1 = -1; // Initialize to -1 to indicate no card selected
-    int card2 = -1;
-    int counterPlayer1 = 0;
-    int counterPlayer2 = 0;
-    boolean player1Turn = true; // Player 1 starts the game
-    ImageView[] imageViewsArray = new ImageView[16];
-    Integer[] drawablesArray = new Integer[16];
+    private int count = 0;
+    private int card1 = -1; // Initialize to -1 to indicate no card selected
+    private int card2 = -1;
+    private int counterPlayer1 = 0;
+    private int counterPlayer2 = 0;
+    private boolean player1Turn = true; // Player 1 starts the game
+    private final ImageView[] imageViewsArray = new ImageView[16];
+    private final Integer[] drawablesArray = new Integer[16];
+    private final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         shuffleDrawablesArray();
     }
 
-    // Fill imageViewsArray with ImageViews
+    // First array - Image views
     private void fillImageViewsArray() {
         for (int i = 0; i < imageViewsArray.length; i++) {
             int imageViewId = getResources().getIdentifier("car" + (i + 1), "id", getPackageName());
@@ -65,22 +42,24 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Fill drawablesArray with drawables identifiers
+    // Second array - Drawables (Images) identifiers
     private void fillDrawablesArray() {
         for (int i = 0; i < drawablesArray.length; i++) {
+            // Fill in the drawable's identifier
             int drawableId = getResources().getIdentifier("img_" + ((i % 8) + 1), "drawable", getPackageName());
             drawablesArray[i] = drawableId;
         }
     }
 
-    // Shuffle drawablesArray
+    // Second array - Shuffle the array
     private void shuffleDrawablesArray() {
-        List<Integer> drawablesList = Arrays.asList(drawablesArray);
-        Collections.shuffle(drawablesList);
+        List<Integer> drawablesList = Arrays.asList(drawablesArray); // Convert array to list
+        Collections.shuffle(drawablesList); // Shuffle list
         drawablesList.toArray(drawablesArray);
+
     }
 
-    public void openCard(View view) {
+    public void openCard(View view) throws InterruptedException {
         ImageView imageView = (ImageView) view;
         int id_of_imageview_in_array = 0;
         for (int i = 0; i < imageViewsArray.length; i++) {
@@ -89,25 +68,14 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
         }
-
-        // If the card is already flipped, return
-        if (imageView.getDrawable() != null) {
-            return;
-        }
-
         imageView.setImageResource(drawablesArray[id_of_imageview_in_array]);
         count++;
-
-        // Store the position of the card clicked
-        if (count == 1) {
-            card1 = id_of_imageview_in_array;
-        } else if (count == 2) {
-            card2 = id_of_imageview_in_array;
-            turnEnd();
-        }
+        turnEnd(imageView);
     }
 
-    private void turnEnd() {
+
+
+    private void turnEnd(ImageView imageView) {
         if (drawablesArray[card1].equals(drawablesArray[card2])) {
             // Match found
             if (player1Turn) {
@@ -116,20 +84,15 @@ public class MainActivity extends AppCompatActivity {
                 counterPlayer2++;
             }
             if (counterPlayer1 + counterPlayer2 == 8) {
-                // Game Over, you can put your game over logic here
-                // For now, let's just display a toast
-                Toast.makeText(this, "Game Over!", Toast.LENGTH_SHORT).show();
+                // Game Over
+                Toast.makeText(this, "Game Over! Player 1: " + counterPlayer1 + " - Player 2: " + counterPlayer2, Toast.LENGTH_LONG).show();
             }
         } else {
             // No match found
-            // Delay hiding cards for better user experience
-            imageViewsArray[card1].postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    imageViewsArray[card1].setImageResource(0); // Hide first card
-                    imageViewsArray[card2].setImageResource(0); // Hide second card
-                }
-            }, 1000);
+            imageViewsArray[card1].setImageResource(R.drawable.back32); // Hide first card
+            imageViewsArray[card2].setImageResource(R.drawable.back32);
+            imageViewsArray[card1].setTag(false);
+            imageViewsArray[card2].setTag(false);
         }
 
         // Reset count and prepare for the next turn
@@ -140,5 +103,10 @@ public class MainActivity extends AppCompatActivity {
         // Switch player turns
         player1Turn = !player1Turn;
     }
+
+    public void onBtnClicked(View view) {
+
+        Log.e("XXXXXXX", "+onBtnClick");
+        }
 }
 
