4a5ddf5c196a95317432efd0bb175539f7c133d5
NirBerman-app <dhdhggffv@gmail.com>
2024-05-17 11:55:37 +0300

    two card left

--- app/src/main/java/com/example/remaindergame/MainActivity.java
+++ app/src/main/java/com/example/remaindergame/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -15,11 +14,11 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private int count = 0;
-    private int card1 = -1; // Initialize to -1 to indicate no card selected
+    private int card1 = -1;
     private int card2 = -1;
     private int counterPlayer1 = 0;
     private int counterPlayer2 = 0;
-    private boolean player1Turn = true; // Player 1 starts the game
+    private boolean player1Turn = true;
     private final ImageView[] imageViewsArray = new ImageView[16];
     private final Integer[] drawablesArray = new Integer[16];
     private final Handler handler = new Handler();
@@ -32,9 +31,44 @@ public class MainActivity extends AppCompatActivity {
         fillImageViewsArray();
         fillDrawablesArray();
         shuffleDrawablesArray();
+
+        // Set onClick listeners for each image view
+        for (ImageView imageView : imageViewsArray) {
+            imageView.setOnClickListener(this::onBtnClicked);
+        }
     }
 
-    // First array - Image views
+    public void onBtnClicked(View view) {
+        ImageView clickedImageView = (ImageView) view;
+
+        int clickedIndex = -1;
+        for (int i = 0; i < imageViewsArray.length; i++) {
+            if (imageViewsArray[i] == clickedImageView) {
+                clickedIndex = i;
+                break;
+            }
+        }
+
+        // If card is already opened, ignore the click
+        if (clickedImageView.getTag() != null && (boolean) clickedImageView.getTag()) {
+            return;
+        }
+
+        clickedImageView.setImageResource(drawablesArray[clickedIndex]);
+        clickedImageView.setTag(true);
+Log.e("xxxx" , "line 59");
+        if (count == 0) {
+            Log.e("xxxx" , "line 61");
+            card1 = clickedIndex;
+            count++;
+        } else if (count == 1) {
+            Log.e("xxxx" , "line 65");
+            card2 = clickedIndex;
+            count++;
+            handler.postDelayed(this::turnEnd, 1000);
+        }
+    }
+
     private void fillImageViewsArray() {
         for (int i = 0; i < imageViewsArray.length; i++) {
             int imageViewId = getResources().getIdentifier("car" + (i + 1), "id", getPackageName());
@@ -42,71 +76,40 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Second array - Drawables (Images) identifiers
     private void fillDrawablesArray() {
         for (int i = 0; i < drawablesArray.length; i++) {
-            // Fill in the drawable's identifier
             int drawableId = getResources().getIdentifier("img_" + ((i % 8) + 1), "drawable", getPackageName());
             drawablesArray[i] = drawableId;
         }
     }
 
-    // Second array - Shuffle the array
     private void shuffleDrawablesArray() {
-        List<Integer> drawablesList = Arrays.asList(drawablesArray); // Convert array to list
-        Collections.shuffle(drawablesList); // Shuffle list
+        List<Integer> drawablesList = Arrays.asList(drawablesArray);
+        Collections.shuffle(drawablesList);
         drawablesList.toArray(drawablesArray);
-
     }
 
-    public void openCard(View view) throws InterruptedException {
-        ImageView imageView = (ImageView) view;
-        int id_of_imageview_in_array = 0;
-        for (int i = 0; i < imageViewsArray.length; i++) {
-            if (imageViewsArray[i] == imageView) {
-                id_of_imageview_in_array = i;
-                break;
-            }
-        }
-        imageView.setImageResource(drawablesArray[id_of_imageview_in_array]);
-        count++;
-        turnEnd(imageView);
-    }
-
-
-
-    private void turnEnd(ImageView imageView) {
+    private void turnEnd() {
         if (drawablesArray[card1].equals(drawablesArray[card2])) {
-            // Match found
             if (player1Turn) {
                 counterPlayer1++;
             } else {
                 counterPlayer2++;
             }
+
             if (counterPlayer1 + counterPlayer2 == 8) {
-                // Game Over
                 Toast.makeText(this, "Game Over! Player 1: " + counterPlayer1 + " - Player 2: " + counterPlayer2, Toast.LENGTH_LONG).show();
             }
         } else {
-            // No match found
-            imageViewsArray[card1].setImageResource(R.drawable.back32); // Hide first card
+            imageViewsArray[card1].setImageResource(R.drawable.back32);
             imageViewsArray[card2].setImageResource(R.drawable.back32);
             imageViewsArray[card1].setTag(false);
             imageViewsArray[card2].setTag(false);
         }
 
-        // Reset count and prepare for the next turn
         count = 0;
         card1 = -1;
         card2 = -1;
-
-        // Switch player turns
         player1Turn = !player1Turn;
     }
-
-    public void onBtnClicked(View view) {
-
-        Log.e("XXXXXXX", "+onBtnClick");
-        }
 }
-
